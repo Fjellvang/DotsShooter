8110596ebda5375f6032de1d83c1d3c0421481f8
Aleksander Fjellvang <afj@dwarf.dk>
2024-09-25 15:47:53 +0200

    introduced playerprefab and rotate towards

---
+++ Assets/Scripts/RotateTowardsComponentAuthoring.cs
@@ -0,0 +1,23 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace DotsShooter
+{
+    public struct RotateTowardsComponent : IComponentData
+    {
+        public float RotationOffset;
+    }
+
+    public class RotateTowardsComponentAuthoring : MonoBehaviour
+    {
+        [SerializeField] public float rotationOffset;
+        public class RotateTowardsComponentBaker : Baker<RotateTowardsComponentAuthoring>
+        {
+            public override void Bake(RotateTowardsComponentAuthoring authoring)
+            {
+                var entity = GetEntity(TransformUsageFlags.Dynamic);
+                AddComponent(entity, new RotateTowardsComponent { RotationOffset = authoring.rotationOffset });
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/RotateTowardsSystem.cs
@@ -0,0 +1,30 @@
+﻿using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+namespace DotsShooter
+{
+    public partial struct RotateTowardsSystem : ISystem
+    {
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state) {
+
+            foreach (var (movingObject, transform, rotateTowardsComponent) in 
+                     SystemAPI.Query<RefRO<MovementComponent>, RefRW<LocalTransform>, RefRO<RotateTowardsComponent>>())
+            {
+                var direction = movingObject.ValueRO.Direction;
+                if (direction.Equals(float3.zero))
+                {
+                    continue;
+                }
+
+                var offset = math.TORADIANS * rotateTowardsComponent.ValueRO.RotationOffset;
+                var angle = math.atan2(direction.y, direction.x);
+                var rotation = quaternion.Euler(0, 0, angle + offset);
+                // TODO: Maybe smoooth it?
+                transform.ValueRW.Rotation = rotation;
+            }
+        }
+    }
+}
