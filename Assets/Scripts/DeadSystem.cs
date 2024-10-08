5de229e0ec921eb7ae55c68efe27bef3bb3c7444
Aleksander <aleksander@fjellvang.dk>
2024-10-08 18:34:37 +0200

    feat: started deadsystem

---
+++ Assets/Scripts/DeadSystem.cs
@@ -0,0 +1,60 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Transforms;
+
+namespace DotsShooter
+{
+    public struct DeadEntities : IComponentData
+    {
+        public NativeQueue<DeadEntity> Value;
+    }
+
+    public struct DeadEntity
+    {
+        public EntityType EntityType;
+        public Entity Entity;
+    }
+
+
+    public enum EntityType
+    {
+        Bullet,
+        Enemy,
+        Player
+    }
+    public partial struct DeadSystem : ISystem
+    {
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+            state.RequireForUpdate<EndSimulationEntityCommandBufferSystem.Singleton>();
+            state.RequireForUpdate<DeadEntities>();
+            
+            var deadEntities = new NativeQueue<DeadEntity>(Allocator.Persistent);
+            state.EntityManager.AddComponentData(state.SystemHandle, new DeadEntities(){Value = deadEntities});
+        } 
+        
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            var deadEntities = SystemAPI.GetSingletonRW<DeadEntities>().ValueRW.Value;
+            var ecbSystem = SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>();
+            var ecb = ecbSystem.CreateCommandBuffer(state.WorldUnmanaged);
+            var childBufferFromEntity = SystemAPI.GetBufferLookup<Child>(true);
+            
+            while (deadEntities.TryDequeue(out var deadEntity))
+            {
+                state.EntityManager.DestroyEntity(deadEntity.Entity);
+                Helpers.DestroyEntityHierarchy(deadEntity.Entity, ref ecb, ref childBufferFromEntity);
+            }
+        }
+        
+        [BurstCompile]
+        public void OnDestroy(ref SystemState state)
+        {
+            var deadEntities = SystemAPI.GetSingletonRW<DeadEntities>();
+            deadEntities.ValueRW.Value.Dispose();
+        }
+    }
+}
