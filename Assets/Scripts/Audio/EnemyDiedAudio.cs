30dd8a3302d633fda7301899bfe017272ca96401
Aleksander <aleksander@fjellvang.dk>
2024-10-10 15:35:50 +0200

    feat: simple Audio

---
+++ Assets/Scripts/Audio/EnemyDiedAudio.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using DotsShooter.Events;
+using DotsShooter.UI;
+using Unity.Entities;
+using UnityEngine;
+using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
+
+namespace DotsShooter.Audio
+{
+    public class EnemyDiedAudio : MonoBehaviour
+    {
+        private AudioSource[] _audioSources;
+        [SerializeField]
+        AudioClip deadSound;
+        
+        [SerializeField]
+        float pitchVariance = 0.1f;
+        [SerializeField]
+        float originalPitch;
+
+        private void Awake()
+        {
+            _audioSources = GetComponentsInChildren<AudioSource>();
+        }
+
+        private AudioSource GetNextAudioSource()
+        {
+            foreach (var audioSource in _audioSources)
+            {
+                if (!audioSource.isPlaying)
+                {
+                    return audioSource;
+                }
+            }
+            return null;
+        }   
+        [ContextMenu("Play")]
+        public void Play()
+        {
+            var audioSource = GetNextAudioSource();
+            if (!audioSource)
+            {
+                return;
+            }
+            var variance = Random.Range(-pitchVariance, pitchVariance);
+            audioSource.clip = deadSound;
+            audioSource.pitch = originalPitch + variance;
+            audioSource.Play();
+        }
+
+        private void OnEnable()
+        {
+            var eventSystem = World.DefaultGameObjectInjectionWorld.GetExistingSystemManaged<EventSystem>();
+            eventSystem.OnEnemyDied += Play;
+        }
+    }
+}
---
+++ Assets/Scripts/Audio/PlayerDiedAudio.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using DotsShooter.Events;
+using DotsShooter.UI;
+using Unity.Entities;
+using UnityEngine;
+using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
+
+namespace DotsShooter.Audio
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class PlayerDiedAudio : MonoBehaviour
+    {
+        private AudioSource _audioSource;
+        private void Awake()
+        {
+            _audioSource = GetComponent<AudioSource>();
+        }
+
+        [ContextMenu("Play")]
+        public void Play()
+        {
+            _audioSource.Play();
+        }
+
+        private void OnEnable()
+        {
+            var eventSystem = World.DefaultGameObjectInjectionWorld.GetExistingSystemManaged<EventSystem>();
+            eventSystem.OnPlayerDied += Play;
+        }
+    }
+}
