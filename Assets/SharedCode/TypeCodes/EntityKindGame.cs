7bbc96f83a578588b56284c491b91f727a1cab32
Aleksander <aleksander@fjellvang.dk>
2025-02-22 13:41:39 +0100

    add persisted item

---
+++ Assets/SharedCode/TypeCodes/EntityKindGame.cs
@@ -0,0 +1,10 @@
+﻿using Metaplay.Core;
+
+namespace Game.Logic.TypeCodes
+{
+    [EntityKindRegistry(100, 300)]
+    public static class EntityKindGame
+    {
+        public static readonly EntityKind Leaderboard = EntityKind.FromValue(100);
+    }
+}
---
+++ Backend/Server/Leaderboard/LeaderboardModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Metaplay.Cloud.Persistence;
+using Metaplay.Core;
+using Metaplay.Core.Model;
+
+namespace Game.Server.Leaderboard;
+
+[MetaSerializable]
+[SupportedSchemaVersions(1,1)]
+public class LeaderboardModel : ISchemaMigratable
+{
+    // [MetaMember(1)]
+    // public List<LeaderboardEntry> Entries = new List<LeaderboardEntry>();
+}
+
+[Table("LeaderboardEntries")]
+public class PersistedLeaderboardModel : IPersistedItem
+{
+    [Key]
+    [PartitionKey]
+    [Required]
+    public int Id { get; set; }
+    
+    [Required]
+    [Column(TypeName = "varchar(64)")]
+    public string PlayerId { get; set; }
+    // Helper accessor for parsing the PlayerId back to an EntityId
+    public EntityId PlayerEntityId => EntityId.ParseFromString(PlayerId);
+    
+    [Required]
+    public int Kills { get; set; }
+    [Required]
+    public int GoldCollected { get; set; }
+}
