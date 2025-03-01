b0379e81696459ea1964c1e8633a1578963aae15
Aleksander <aleksander@fjellvang.dk>
2025-03-01 15:29:21 +0100

    migrate to playerid as primary key

---
+++ Backend/Server/Migrations/20250301142515_RemoveId.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_LeaderboardEntries",
+                table: "LeaderboardEntries");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "LeaderboardEntries");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_LeaderboardEntries",
+                table: "LeaderboardEntries",
+                column: "PlayerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_LeaderboardEntries",
+                table: "LeaderboardEntries");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "LeaderboardEntries",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_LeaderboardEntries",
+                table: "LeaderboardEntries",
+                column: "Id");
+        }
+    }
+}
