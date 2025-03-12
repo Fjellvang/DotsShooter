e33591b2ca428d863f09d36556b62bbd49773b92
Aleksander <aleksander@fjellvang.dk>
2025-03-12 11:49:54 +0100

    refactor more systems to jobns

--- Assets/Scripts/Movement/RotateTowardsSystem.cs
+++ Assets/Scripts/Movement/RotateTowardsSystem.cs
@@ -1,33 +1,53 @@
-﻿using Unity.Burst;
+using Unity.Burst;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
 
 namespace DotsShooter
 {
+    [BurstCompile]
     public partial struct RotateTowardsSystem : ISystem
     {
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+            state.RequireForUpdate<MovementComponent>();
+            state.RequireForUpdate<RotateTowardsComponent>();
+        }
+
         [BurstCompile]
         public void OnUpdate(ref SystemState state)
         {
             var deltaTime = SystemAPI.Time.DeltaTime;
-            foreach (var (movingObject, transform, rotateTowardsComponent) in 
-                     SystemAPI.Query<RefRO<MovementComponent>, RefRW<LocalTransform>, RefRO<RotateTowardsComponent>>())
+            
+            // Schedule the job
+            new RotateTowardsJob
             {
-                var direction = movingObject.ValueRO.Direction;
-                if (direction.Equals(float3.zero))
-                {
-                    continue;
-                }
+                DeltaTime = deltaTime
+            }.ScheduleParallel();
+        }
+    }
 
-                var offset = math.TORADIANS * rotateTowardsComponent.ValueRO.RotationOffset;
-                var angle = math.atan2(direction.y, direction.x);
-                var targetRotation = quaternion.Euler(0, 0, angle + offset);
-                var speed = rotateTowardsComponent.ValueRO.RotationSpeed;
-                var rotation = math.slerp(transform.ValueRO.Rotation, targetRotation, deltaTime * speed);
-                // TODO: Maybe smoooth it?
-                transform.ValueRW.Rotation = rotation;
+    [BurstCompile]
+    public partial struct RotateTowardsJob : IJobEntity
+    {
+        public float DeltaTime;
+        
+        [BurstCompile]
+        public void Execute(in MovementComponent movementComponent, ref LocalTransform transform, in RotateTowardsComponent rotateTowardsComponent)
+        {
+            var direction = movementComponent.Direction;
+            if (math.all(direction == float3.zero))
+            {
+                return;
             }
+
+            var offset = math.TORADIANS * rotateTowardsComponent.RotationOffset;
+            var angle = math.atan2(direction.y, direction.x);
+            var targetRotation = quaternion.Euler(0, 0, angle + offset);
+            var speed = rotateTowardsComponent.RotationSpeed;
+            var rotation = math.slerp(transform.Rotation, targetRotation, DeltaTime * speed);
+            transform.Rotation = rotation;
         }
     }
 }
--- Assets/Scripts/Movement/TrackPlayerSystem.cs
+++ Assets/Scripts/Movement/TrackPlayerSystem.cs
@@ -3,37 +3,56 @@ using Unity.Burst;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
+using Unity.Collections;
 
 namespace DotsShooter
 {
     [UpdateBefore(typeof(MovementSystem))]
+    [BurstCompile]
     public partial struct TrackPlayerSystem : ISystem
     {
+        [BurstCompile]
         public void OnCreate(ref SystemState state)
         {
             state.RequireForUpdate<PlayerTag>();
+            state.RequireForUpdate<TrackPlayerComponent>();
         }
 
         [BurstCompile]
-        public void OnUpdate(ref SystemState state) {
+        public void OnUpdate(ref SystemState state)
+        {
             var playerEntity = SystemAPI.GetSingletonEntity<PlayerTag>();
             var playerPosition = SystemAPI.GetComponent<LocalTransform>(playerEntity).Position;
-            foreach (var (movement, transform) in 
-                     SystemAPI.Query<RefRW<MovementComponent>, RefRO<LocalTransform>>()
-                         .WithAll<TrackPlayerComponent>())
+
+            // Schedule the job with captured player position
+            new TrackPlayerJob
             {
-                var direction = playerPosition - transform.ValueRO.Position;
-                var distance = math.length(direction);
-                if (distance > 0.1f)
-                {
-                    direction /= distance;
-                    movement.ValueRW.Direction = direction;
-                }
-                else
-                {
-                    movement.ValueRW.Direction = float3.zero;
-                }
+                PlayerPosition = playerPosition
+            }.ScheduleParallel();
+        }
+    }
+
+    [BurstCompile]
+    public partial struct TrackPlayerJob : IJobEntity
+    {
+        [ReadOnly] 
+        public float3 PlayerPosition;
+
+        [BurstCompile]
+        public void Execute(ref MovementComponent movement, in LocalTransform transform, in TrackPlayerComponent trackPlayer)
+        {
+            var direction = PlayerPosition - transform.Position;
+            var distance = math.length(direction);
+            
+            if (distance > 0.1f)
+            {
+                direction /= distance;
+                movement.Direction = direction;
             }
-        } 
+            else
+            {
+                movement.Direction = float3.zero;
+            }
+        }
     }
 }
