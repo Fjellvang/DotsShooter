f461d1a5f00ea9fab82bf716d45b5f4578d8b76e
Aleksander <aleksander@fjellvang.dk>
2025-03-16 12:48:58 +0100

    deleted old shooting system

--- Assets/Scripts/Player/Stats/StatsSystem.cs
+++ Assets/Scripts/Player/Stats/StatsSystem.cs
@@ -20,20 +20,20 @@ namespace DotsShooter.Player
 
         protected override void OnUpdate()
         {
-            var statsData = MetaplayClient.PlayerModel.GameStats;
-            foreach (var (autoShooting, movement, autoTargetingPlayer, entity) in
-                     SystemAPI.Query<RefRW<AutoShootingComponent>, RefRW<MovementSpeedComponent>, RefRW<AutoTargetingPlayer>>()
-                         .WithAll<PlayerTag, StatsNeedsInitialization>()
-                         .WithEntityAccess())
-            {
-                autoShooting.ValueRW.ProjectileDamage = statsData.Damage.Float;
-                autoShooting.ValueRW.Cooldown = statsData.AttackSpeed.Float;
-                autoShooting.ValueRW.ProjectileRadius = statsData.ExplosionRadius.Float;
-                movement.ValueRW.Speed = statsData.MoveSpeed.Float;
-                autoTargetingPlayer.ValueRW.Range = statsData.Range.Float;
-                
-                SystemAPI.SetComponentEnabled<StatsNeedsInitialization>(entity, false);
-            }
+        //     var statsData = MetaplayClient.PlayerModel.GameStats;
+        //     foreach (var (autoShooting, movement, autoTargetingPlayer, entity) in
+        //              SystemAPI.Query<RefRW<AutoShootingComponent>, RefRW<MovementSpeedComponent>, RefRW<AutoTargetingPlayer>>()
+        //                  .WithAll<PlayerTag, StatsNeedsInitialization>()
+        //                  .WithEntityAccess())
+        //     {
+        //         autoShooting.ValueRW.ProjectileDamage = statsData.Damage.Float;
+        //         autoShooting.ValueRW.Cooldown = statsData.AttackSpeed.Float;
+        //         autoShooting.ValueRW.ProjectileRadius = statsData.ExplosionRadius.Float;
+        //         movement.ValueRW.Speed = statsData.MoveSpeed.Float;
+        //         autoTargetingPlayer.ValueRW.Range = statsData.Range.Float;
+        //         
+        //         SystemAPI.SetComponentEnabled<StatsNeedsInitialization>(entity, false);
+        //     }
         }
     }
 }
